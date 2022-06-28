b13651262b0c699dd4291ded0721494fa69ffbf2
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-06-28 18:37:08 +0000

    martes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("this is working!");
+  document.querySelector('.card').classList.add('spade')
+  setTimeount(() => {
+    document.querySelector('.card').classList.add('spade');
+  }, 1000);
 };
+
+let generateRandomNumber(){
+  let numb = Math.floor(Math.random() * 12);
+  let numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+}
