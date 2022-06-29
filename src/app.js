240ed40bd0915c49fd2a0bb9657fe3b9b357ee79
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-06-29 14:44:15 +0000

    miercoles

--- src/app.js
+++ src/app.js
@@ -7,13 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = () => {
   //write your code here
-  document.querySelector('.card').classList.add('spade')
-  setTimeount(() => {
-    document.querySelector('.card').classList.add('spade');
-  }, 1000);
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
 
-let generateRandomNumber(){
-  let numb = Math.floor(Math.random() * 12);
-  let numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
-}
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["spade", "heart", "club", "diamond"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
